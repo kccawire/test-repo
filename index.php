a16bedbcfa64295a1267ca41da7c2cb19ec63854
Plutocrat Andrew <python@caramarket.com>
2018-08-05 20:14:35 +0300

    APIs for PHP and Golang

---
+++ index.php
@@ -0,0 +1,4 @@
+
+<html>
+<head><title>Welcome to Test Page</title></head>
+<body>Welcome Home</body>
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	_ "math"
+)
+
+var value = 6
+
+func main() {
+	name := "Moses"
+	lname := "Neo"
+
+	fmt.Printf("my name is %s %s\n", name, lname)
+	fmt.Printf("my name is %s %s\n", name, lname)
+}
